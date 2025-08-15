26428736b44725f2d682e3cebf43fda2e81920b3
ww910 <108930679+ww910@users.noreply.github.com>
2025-08-16 02:48:50 +0800

    update

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,6 +1,6 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "./config";
-import {Client as QstashClient} from "@upstash/qstash";
+import {Client as QStashClient, resend} from "@upstash/qstash";
 import emailjs from "@emailjs/browser"
 
 export const workflowClient = new WorkflowClient({
@@ -8,24 +8,29 @@ export const workflowClient = new WorkflowClient({
   token: config.env.upstash.qstashToken,
 });
 
-export async function sendEmail({
-  email, subject, message,
-}: { email: string; subject: string; message: string }) {
-  const res = await fetch("https://api.emailjs.com/api/v1.0/email/send", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    // For server usage, prefer a private key (ACCESS TOKEN) from EmailJS
-    body: JSON.stringify({
-      service_id: 'service_u5vbi42',
-      template_id: 'template_tdqxoah',
-      user_id: "LwWXTEu8hRY_80-og",     // or omit if using private key only
-      accessToken: process.env.EMAILJS_PRIVATE_KEY, // recommended for server
-      template_params: { email, subject, message },
-    }),
-  });
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
 
-  if (!res.ok) {
-    const text = await res.text();
-    throw new Error(`EmailJS failed: ${res.status} ${text}`);
-  }
-}
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Acme <onboarding@resend.dev>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
