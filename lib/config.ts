a1818ec595bd5568701386d8211765675d4dc74d
ww910 <108930679+ww910@users.noreply.github.com>
2025-08-16 02:52:13 +0800

    update

--- lib/config.ts
+++ lib/config.ts
@@ -12,7 +12,7 @@ const config = {
         redisUrl: process.env.UPSTASH_REDIS_URL!,
         redisToken: process.env.UPSTASH_REDIS_TOKEN!,
         qstashUrl: process.env.QSTASH_URL!,
-        qstashToken:process.env.QSTASH_URL_TOKEN!,
+        qstashToken:process.env.QSTASH_TOKEN!,
     },
     resendToken: process.env.RESEND_TOKEN!,
   },
